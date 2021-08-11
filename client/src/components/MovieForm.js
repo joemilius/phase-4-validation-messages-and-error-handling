301d1e66d16dc3db78dfa90972ef97f9ba667dab
JoeMilius <joemilius@gmail.com>
2021-08-11 06:26:49 -0600

    Completed Assignment

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import styled from "styled-components";
 
 function MovieForm() {
+  const [errors, setErrors] = useState([])
   const [formData, setFormData] = useState({
     title: "",
     year: new Date().getFullYear(),
@@ -16,15 +17,26 @@ function MovieForm() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    fetch("/movies", {
+    const response = await fetch("/movies", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(formData),
     })
-      .then((response) => response.json())
-      .then((newMovie) => console.log(newMovie));
+    const data = await response.json()
+    if (response.ok) {
+      console.log("Movie created:", data)
+    } else {
+      setErrors(data.errors)
+    }
+    // .then((response) => {
+    //     if(response.ok) {
+    //       response.json().then(newMovie => console.log(newMovie))
+    //     } else {
+    //       response.json().then(errorData => setErrors(errorData.errors))
+    //     }
+    // })
   }
 
   function handleChange(e) {
@@ -127,6 +139,13 @@ function MovieForm() {
         </FormGroup>
         <SubmitButton type="submit">Add Movie</SubmitButton>
       </form>
+      {errors.length > 0 && (
+        <ul style={{ color: "red" }}>
+          {errors.map((error) => (
+            <li key={error}>{error}</li>
+          ))}
+        </ul>
+      )}
     </Wrapper>
   );
 }
